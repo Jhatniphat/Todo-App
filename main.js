7990bc0506ec4a0f3dd0f094cf224d873be833a2
Jhatniphat Sarakal <52996903+Jhatniphat@users.noreply.github.com>
2023-12-07 12:09:42 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 import {
-  // addTodoHandler,
-  beforeUnloadHandler,
-  loadHandler
-} from './eventHandler/eventController.js'
+  todoManagement
+} from "./lib/todoManagement.js"
+const {
+    addTodo,
+    removeTodo,
+    findTodo,
+    getTodos,
+    getNumberOfDone,
+    getNumberOfNotDone,
+    clearTodo,
+    setDone,
+    setItemToDone
+  } = todoManagement()
+import {showTodoItem , showNumberOfDone , showNumberOfNotDone , removeTodoItem} from "./UI/todoList.js"
+import {
+  addTodoHandler,
+  notDoneButtonHandler,
+  removeButtonHandler,
+  beforeUnloadHandler
+} from "./eventHandler/eventController.js"
 
-window.addEventListener('load', () => {
-  loadHandler()
+const addButton = document.getElementById('addBtn')
+addButton.addEventListener('click', () => addTodoHandler())
+
+const inputTodo = document.querySelector('input')
+inputTodo.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') addTodoHandler()
 })
-window.addEventListener('beforeunload', (event) => {
-  beforeUnloadHandler(event)
-})
 
-// const addButton = document.getElementById('addBtn')
-// addButton.addEventListener('click', () => addTodoHandler())
-
-// const inputTodo = document.querySelector('input')
-// inputTodo.addEventListener('keypress', (event) => {
-//   if (event.key === 'Enter') addTodoHandler()
-// })
